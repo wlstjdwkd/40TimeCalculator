d8227c3a3056e28dc5d797512f35769e2c5a3f90
youhyeoneee <khukyh2018@khu.ac.kr>
2025-01-29 12:15:19 +0900

    design : modify DaySection css

--- frontend/time_calculator/src/components/DaySection/DaySection.js
+++ frontend/time_calculator/src/components/DaySection/DaySection.js
@@ -7,31 +7,44 @@ import './DaySection.css'; // 스타일을 별도의 CSS 파일로 관리
 function DaySection({ imageSrc, startTime, endTime, totalTime, onStartChange, onEndChange }) {
   return (
     <div className="day-section">
+      {/* 요일 이미지 */}
       <img src={imageSrc} alt="day" />
+
+      {/* 출근, 퇴근, 근무시간 */}
       <div className="day-section-main">
         <div className="day-section-timepickers">
-          <TimePicker
-            onChange={onStartChange}
-            value={startTime}
-            format="HH:mm:ss"
-            clearIcon={null}
-            maxDetail="second"
-            disableClock
-            aria-label="출근 시간"
-            className="time-picker"
-          />
-          <span className="tilde">~</span>
-          <TimePicker
-            onChange={onEndChange}
-            value={endTime}
-            format="HH:mm:ss"
-            clearIcon={null}
-            maxDetail="second"
-            disableClock
-            aria-label="퇴근 시간"
-            className="time-picker"
-          />
+          {/* 출근 시간 */}
+          <div className="time-picker">
+            <TimePicker
+              onChange={onStartChange}
+              value={startTime}
+              format="HH:mm:ss"
+              clearIcon={null}
+              maxDetail="second"
+              disableClock
+              aria-label="출근 시간"
+            />            
+          </div>
+
+
+          {/* ~ 기호 */}
+          <div className="tilde">~</div>
+
+          <div className="time-picker">
+            {/* 퇴근 시간 */}
+            <TimePicker
+              onChange={onEndChange}
+              value={endTime}
+              format="HH:mm:ss"
+              clearIcon={null}
+              maxDetail="second"
+              disableClock
+              aria-label="퇴근 시간"
+            />
+          </div>
         </div>
+
+        {/* 근무 시간 */}
         <div className="day-section-total">
           {totalTime}
         </div>
