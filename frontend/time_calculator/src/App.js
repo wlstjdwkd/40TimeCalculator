e21d846598a19156fa341b49d3c482eb99a509cf
wlstjd <xkakrlfh@naver.com>
2025-02-09 16:41:52 +0900

    fix : logo icon change

--- frontend/time_calculator/src/App.js
+++ frontend/time_calculator/src/App.js
@@ -7,10 +7,7 @@ import DaySection from './components/DaySection/DaySection';
 import SummaryBox from './components/SummaryBox/SummaryBox';
 import WorkSettings from './components/WorkSettings/WorkSettings';
 import Footer from './components/Footer/Footer';
-// import { toZonedTime, fromZonedTime } from 'date-fns-tz';
-// import { format, startOfWeek, addWeeks, differenceInMilliseconds } from 'date-fns';
 
-// const TIMEZONE = 'Asia/Seoul';
 const STORAGE_KEY = 'worktime-calculator-data';
 
 const dayImages = {
@@ -46,68 +43,6 @@ function App() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 	const togglePopup = () => setIsPopupOpen((prev) => !prev);
 
-  // // 주간 데이터 초기화 함수
-  // const resetWeeklyData = useCallback(() => {
-  //   // 상태 초기화
-  //   setDaysData(initialDaysData);
-
-  //   // 로컬 스토리지에 데이터 업데이트
-  //   const dataToSave = {
-  //     requiredWorkingTime,
-  //     coreTimeStart,
-  //     coreTimeEnd,
-  //     lunchBreakTime,
-  //     daysData: initialDaysData,
-  //   };
-  //   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataToSave));
-
-  //   // 초기화 메시지 설정
-  //   alert('월요일이 되었으므로 시간이 초기화되었습니다.');
-  // }, [requiredWorkingTime, coreTimeStart, coreTimeEnd, lunchBreakTime]);
-
-  // // 초기화 로직 및 타이머 설정
-  // useEffect(() => {
-  //   try {
-  //     const now = new Date();
-
-  //     // 현재 시간을 KST로 변환
-  //     const zonedNow = toZonedTime(now, TIMEZONE);
-
-  //     // 이번 주 월요일 시작 (0시)
-  //     const currentWeekStart = startOfWeek(zonedNow, { weekStartsOn: 1 });
-  //     const formattedStartOfWeek = format(currentWeekStart, 'yyyy-MM-dd');
-
-  //     const lastReset = localStorage.getItem('lastReset');
-
-  //     // 로컬 스토리지 초기화 확인
-  //     if (lastReset !== formattedStartOfWeek) {
-  //       resetWeeklyData();
-  //       localStorage.setItem('lastReset', formattedStartOfWeek);
-  //     }
-
-  //     // 다음 주 월요일 0시까지 남은 시간 계산
-  //     const nextWeekStart = addWeeks(currentWeekStart, 1);
-  //     const nextMondayStartZoned = toZonedTime(nextWeekStart, TIMEZONE);
-  //     nextMondayStartZoned.setHours(0, 0, 0, 0);
-
-  //     const nextMondayUtc = fromZonedTime(nextMondayStartZoned, TIMEZONE);
-  //     const nowUtc = fromZonedTime(zonedNow, TIMEZONE);
-  //     const timeUntilNextMonday = differenceInMilliseconds(nextMondayUtc, nowUtc);
-
-  //     // 타이머 설정
-  //     const timer = setTimeout(() => {
-  //       resetWeeklyData();
-  //       const newStartOfWeek = startOfWeek(new Date(), { weekStartsOn: 1 });
-  //       const newFormattedStartOfWeek = format(newStartOfWeek, 'yyyy-MM-dd');
-  //       localStorage.setItem('lastReset', newFormattedStartOfWeek);
-  //     }, timeUntilNextMonday);
-
-  //     // 타이머 정리
-  //     return () => clearTimeout(timer);
-  //   } catch (error) {
-  //     console.error('초기화 로직 오류:', error);
-  //   }
-  // }, []);
 
   //화면 새로 들어갈 때마다 새로고침하는 로직
   useEffect(() => {
